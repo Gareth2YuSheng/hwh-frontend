5994a0d11194f75e3d4edaf4f9c657ef718ae82c
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-01-22 15:58:08 +0800

    Update httpRequests.ts
    Changed to API Gateway URL

--- src/helpers/httpRequests.ts
+++ src/helpers/httpRequests.ts
@@ -1,5 +1,6 @@
-// const baseUrl = `http://localhost:8080`;
-const baseUrl = `https://3.88.22.170:8080`;
+// const baseUrl = `http://localhost:8080`; //Localhost
+// const baseUrl = `https://3.88.22.170:8080`; //EC2 Directly
+const baseUrl = `https://my9aw9ky3e.execute-api.us-east-1.amazonaws.com/dev/`; //AWS API GATEWAY
 
 export async function getRequest(url: string, headers = {}) {
   return fetch(baseUrl + url, {
